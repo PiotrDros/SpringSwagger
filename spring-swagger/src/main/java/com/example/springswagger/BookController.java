84e82040174118e0f0957f7363b93590504aab25
Piotr Dros <piotrdros@gmail.com>
2018-11-15 16:30:00 +0100

    Add gradle support

--- spring-swagger/src/main/java/com/example/springswagger/BookController.java
+++ spring-swagger/src/main/java/com/example/springswagger/BookController.java
@@ -1,5 +1,7 @@
 package com.example.springswagger;
 
+import java.util.stream.Collectors;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -23,10 +25,16 @@ public class BookController {
     static {
         Book book = new Book();
         book.setIsbn("ISBN 9780071808552");
-        book.setTitle("Java: The Complete Reference, Ninth Edition ");
+        book.setTitle("Java: The Complete Reference, Ninth Edition");
         book.getAuthors().add("Herbert Schildt");
-
         books.add(book);
+
+        Book book2 = new Book();
+        book2.setIsbn("ISBN 9781904808442");
+        book2.setTitle("Alice in Wonderland");
+        book2.getAuthors().add("Lewis Carroll");
+        books.add(book2);
+
     }
 
     @ApiOperation(value = "Adds book")
@@ -42,9 +50,15 @@ public class BookController {
     @ApiOperation(value = "Get books")
     @ApiResponses(value = { @ApiResponse(code = 200, message = "Books retrieved", response = ListOfBooks.class) })
     @RequestMapping(value = "books", method = RequestMethod.GET)
-    public List<Book> getBooks() {
+    public List<Book> getBooks(@RequestParam(required =  false) String filter) {
         System.out.println("Get books");
-        return books;
+
+        if (StringUtils.isEmpty(filter)) {
+            return books;
+        } else {
+            return books.stream().filter(b -> b.getTitle().contains(filter)).collect(Collectors.toList());
+        }
+
     }
 
 }
